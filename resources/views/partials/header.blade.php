225f05f3239ceeb325fcef197555a85762660922
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-24 17:46:58 +0100

    link nel menù e classe active

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,34 @@
 <header>    
     <nav class="main-nav">
         <ul class='main-menu flex list-style-none'>
-            <li class="menu-item"><a href="#" class="menu-item-link">Home page</a></li>
-            <li class="menu-item"><a href="#" class="menu-item-link">Pacchetti Viaggi</a></li>
-            <li class="menu-item"><a href="#" class="menu-item-link">Destinazioni</a></li>
-            <li class="menu-item"><a href="#" class="menu-item-link">Contattaci</a></li>
+            <li class="menu-item">
+                <a href="{{route('home')}}" class="menu-item-link
+                 @if (Request::route()->getName()==='home') active
+                @endif">
+                Home page
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="{{route('pacchetti')}}" class="menu-item-link 
+                @if (Request::route()->getName()==='pacchetti') active
+                @endif">
+                    Pacchetti Viaggi
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="{{route('destinazioni')}}" class="menu-item-link 
+                @if (Request::route()->getName()==='destinazioni') active
+                @endif">
+                    Destinazioni
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="{{route('contatti')}}" class="menu-item-link 
+                @if (Request::route()->getName()==='contatti') active
+                @endif">
+                    Contattaci
+                </a>
+            </li>
         </ul>
     </nav>
 </header>
