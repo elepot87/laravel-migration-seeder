06f4fe0d6e0088765292af2723d742009bc25a2f
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-24 19:37:51 +0100

    migrate della mia tabella

---
+++ database/migrations/2022_01_24_172218_create_travels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('destination', 50);
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->string('description', 300);
+            $table->tinyInteger('days')->unsigned();
+            $table->boolean('is_available')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
