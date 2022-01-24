041a06fe642aae56d7fd46ffd43aa8ab51d0c6cd
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-24 21:07:20 +0100

    utilizzo di faker per popolare le tabelle

--- database/seeds/TravelsTableSeeder.php
+++ database/seeds/TravelsTableSeeder.php
@@ -16,7 +16,7 @@ class TravelsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //Creazione 20 pacchetti di viaggio (rows)
         for ($i=0; $i < 20; $i++) { 
@@ -24,15 +24,23 @@ class TravelsTableSeeder extends Seeder
             $new_travel = new Travel();
 
             // Popolazione delle colonne
-            $new_travel->name = 'Paradiso innevato';
-            $new_travel->destination = 'Cervinia';
-            $new_travel->departure_date = '2022-02-5';
-            $new_travel->return_date = '2022-2-12';
-            $new_travel->description = 'lorem ipsum dolor sit amet, lorem ipsum dolor sit amet';
-            $new_travel->days = 8;
+            // $new_travel->name = 'Paradiso innevato';
+            // $new_travel->destination = 'Cervinia';
+            // $new_travel->departure_date = '2022-02-5';
+            // $new_travel->return_date = '2022-2-12';
+            // $new_travel->description = 'lorem ipsum dolor sit amet, lorem ipsum dolor sit amet';
+            // $new_travel->days = 8;
+
+            //Usiamo FAKERS
+            $new_travel->name = $faker->text(30);
+            $new_travel->destination = $faker->text(12);
+            $new_travel->departure_date = $faker->dateTimeBetween('+1 week', '+2 week');
+            $new_travel->return_date = $faker->dateTimeBetween('+2 week', '+3 week');
+            $new_travel->description = $faker->paragraph(3);
+            $new_travel->days = $faker->numberBetween(0, 16);
+            // Salvare i dati nel DB
+            $new_travel->save();
         }
 
-        // Salvare i dati nel DB
-        $new_travel->save();
     }
 }
