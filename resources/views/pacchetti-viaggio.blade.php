ef6ee307ffc296e88109b9851e33e0137db90305
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-24 23:20:19 +0100

    aggiunto un po' di stile per pacchettiviaggio.blade

--- resources/views/pacchetti-viaggio.blade.php
+++ resources/views/pacchetti-viaggio.blade.php
@@ -4,15 +4,20 @@
 
 @section('main-content')
     <div class="container-main flex packages-travel">
-    <h1>Pacchetti di viaggio</h1>
-    <h2>Top ten</h2>
-
-    <ul class="list-travels list-style-none">
-        @foreach ($travels as $travel)
-        <li class="travel-item">
-            {{$travel->name}}
-        </li>
-        @endforeach
-    </ul>
+        <h1>Pacchetti di viaggio</h1>
+        <h2>Top ten</h2>
+        <ul class="list-travels list-style-none">
+            @foreach ($travels as $travel)
+            <li class="travel-item">
+                <div class="details">
+                    {{$travel->name}} | Destinazione: {{$travel->destination}} |
+                    Data di partenza: {{$travel->departure_date}} | Giorni: {{$travel->days}}
+                </div>
+            </li>
+            @if(! $loop->last)
+            <hr>
+            @endif
+            @endforeach
+        </ul>
 </div>
 @endsection
