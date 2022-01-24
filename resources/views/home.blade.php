1736544cee4654a8524f9e91b65d882266c89b5a
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-24 17:20:30 +0100

    prima struttura e stile per header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Home page</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+
+@section('head-title', 'VP Travel Home' )
+
+@section('main-content')
+    <h1>Home Page</h1>
+@endsection
+
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>    
+    <nav class="main-nav">
+        <ul class='main-menu flex list-style-none'>
+            <li class="menu-item"><a href="#" class="menu-item-link">Home page</a></li>
+            <li class="menu-item"><a href="#" class="menu-item-link">Pacchetti Viaggi</a></li>
+            <li class="menu-item"><a href="#" class="menu-item-link">Destinazioni</a></li>
+            <li class="menu-item"><a href="#" class="menu-item-link">Contattaci</a></li>
+        </ul>
+    </nav>
+</header>
